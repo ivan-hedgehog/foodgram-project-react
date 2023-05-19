d837d1d2eeb6830317f1b01e46c821629150690a
Ильин Иван <ezikov163@gmail.com>
2023-05-20 01:28:35 +0400

    заполняем базу

--- backend/foodgram_api/recipes/management/commands/csv_load_ingredients_data.py
+++ backend/foodgram_api/recipes/management/commands/csv_load_ingredients_data.py
@@ -1,21 +1,37 @@
-
 from django.core.management.base import BaseCommand
 import json
+import os
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
+    help = 'Add data to bd'
+    
     def handle(self, *args, **options):
 
-        with open('ingredients.json', 'rb') as f:
+        with open('recipes/data/ingredients.json', 'r', encoding='utf-8') as f:
             data = json.load(f)
 
-            for x, y in data.items():
-                print(x)
-                print(y)
-                ingredient = Ingredient()
-                ingredient.name = x
-                ingredient.measurement_unit = y
-                ingredient.save()
-                # s.genres.add(g)
+            for ingredients in data:
+                name = ingredients['name']
+                measurement_unit = ingredients['measurement_unit']
+                Ingredient.objects.create(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
         print('finished')
+
+
+# def load_data_from_json(file_path):
+#     with open (file_path, 'r', encoding='utf-8') as file:
+#         data = json.load(file)
+#         for item in data:
+#             ingredient = Ingredient.objects.create(
+#                 name=item['name'],
+#                 measurement_unit=item['measurement_unit'],
+#             )
+
+# json_file_path = 'recipes/data/ingredients.json'
+
+# load_data_from_json(json_file_path)
+
