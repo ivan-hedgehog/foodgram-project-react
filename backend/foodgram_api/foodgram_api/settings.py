0e9c2025de8c1a71c63cc3ead4ebb023c5d95d78
Иван Ильин <ezikov163@gmail.com>
2023-05-31 11:03:18 +0300

    проверка2

--- backend/foodgram_api/foodgram_api/settings.py
+++ backend/foodgram_api/foodgram_api/settings.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR.parent.parent, 'infra/.env'), verbose=True)
+load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+# load_dotenv(os.path.join(BASE_DIR.parent.parent, 'infra/.env'), verbose=True)
 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
